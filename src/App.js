bad8edea655cd801031c6a29d97b9b0c8812f57d
asendice <dylan.travis23@outlook.com>
2022-06-23 21:22:26 -0700

    tweet state and useEffect for api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './app.module.css';
+import { getTweets } from './apis/auth';
 
 export default function App() {
-  return (
-    <div className={classes.container}>
+  const [tweets, setTweets] = useState([]);
 
-    </div>
-  );
+  useEffect(() => {
+    getTweets().then((response) => {
+      setTweets(response.data.results);
+    });
+  }, []);
+
+  return <div className={classes.container}></div>;
 }
